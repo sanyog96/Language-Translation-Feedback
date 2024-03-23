c3339463498d99fe9cdf3752a150e45276824e89
sanyog96 <sanyog96@gmail.com>
2024-03-23 14:55:13 +0530

    adding kannada lambani and lambani kannada files

--- app/urls.py
+++ app/urls.py
@@ -5,8 +5,12 @@ from django.conf import settings
 
 urlpatterns = [
     path('evaluate', views.evaluate, name='evaluate'),
+    path('kannada_lambani', views.kannada_lambani, name='kannada_lambani'),
+    path('lambani_kannada', views.lambani_kannada, name='lambani_kannada'),
     path('audio', views.audio, name='audio'),
     path('saveText', views.saveText, name='saveText'),
+    path('saveKannadaLambani', views.saveKannadaLambani, name='saveKannadaLambani'),
+    path('saveLambaniKannada', views.saveLambaniKannada, name='saveLambaniKannada'),
     path('saveAudio', views.saveAudio, name='saveAudio'),
     path('rating', views.rating, name='rating')
 ]
--- app/views.py
+++ app/views.py
@@ -32,6 +32,46 @@ def evaluate(request):
     }
     return render(request, "evaluate.html", context)
 
+def kannada_lambani(request):
+    # Map the list with a key value `data`
+    # users = pd.read_csv("D:\S2ST\Language Translation Feedback\static\dataText.csv", sep='|')
+    users = pd.read_csv(settings.STATIC_ROOT + "dataKannadaLambani.csv", sep='|')
+    n = users.shape[0]
+    ids = random.sample(range(0, n), 10)
+    items = []
+    for id in ids:
+        item_dict = dict()
+        item_dict['id'] = id
+        item_dict['kannada_source'] = users.iloc[id]['kannada_source']
+        item_dict['lambani_predicted'] = users.iloc[id]['lambani_predicted']
+        item_dict['lambani_target'] = users.iloc[id]['lambani_target']
+        items.append(item_dict)
+
+    context = {
+        "datas" : items
+    }
+    return render(request, "kannada_lambani.html", context)
+
+def lambani_kannada(request):
+    # Map the list with a key value `data`
+    # users = pd.read_csv("D:\S2ST\Language Translation Feedback\static\dataText.csv", sep='|')
+    users = pd.read_csv(settings.STATIC_ROOT + "dataLambaniKannada.csv", sep='|')
+    n = users.shape[0]
+    ids = random.sample(range(0, n), 10)
+    items = []
+    for id in ids:
+        item_dict = dict()
+        item_dict['id'] = id
+        item_dict['lambani_source'] = users.iloc[id]['lambani_source']
+        item_dict['kannada_predicted'] = users.iloc[id]['kannada_predicted']
+        item_dict['kannada_target'] = users.iloc[id]['kannada_target']
+        items.append(item_dict)
+
+    context = {
+        "datas" : items
+    }
+    return render(request, "lambani_kannada.html", context)
+
 def audio(request):
     # Map the list with a key value `data`
     # users = pd.read_csv("D:\S2ST\Language Translation Feedback\static\dataAudio.csv", sep='|')
@@ -98,6 +138,94 @@ def saveText(request):
     
     return render(request, "saveText.html", context)
 
+def saveKannadaLambani(request):
+    if request.method == 'POST':
+        # with open('D:\S2ST\Language Translation Feedback\media\/text.json', 'r') as f1:
+        with open(settings.MEDIA_ROOT + 'Kannada_Lambani.json', 'r') as f1:
+            json_object = json.load(f1)
+
+        values_dict = {
+            'option1': 1, 
+            'option2': 2, 
+            'option3': 3, 
+            'option4': 4, 
+            'option5': 5
+            }
+        response = request.POST.dict()
+        result = dict()
+        text_total = 0
+        counter = 0
+        for key, value in response.items():
+            if key != 'csrfmiddlewaretoken':
+                if 'text' in key:
+                    text_total += values_dict[value]
+                    counter += 1
+                result[key] = values_dict[value]
+        result['average_text_rating'] = float(text_total)/float(counter)
+        json_object['count'] = json_object['count'] + 1
+        json_object['users'].append(result)
+        total_text_rating = 0
+        for user in json_object['users']:
+            total_text_rating += user['average_text_rating']
+        items = dict()
+        json_object["overall_average_text_rating"] = float(total_text_rating)/float(json_object['count'])
+
+        # with open('D:\S2ST\Language Translation Feedback\media\/text.json', 'w') as f1:
+        with open(settings.MEDIA_ROOT + 'Kannada_Lambani.json', 'w') as f1:
+            json.dump(json_object, f1)
+        
+        items['counter'] = json_object['count']
+        items["overall_average_text_rating"] = round(json_object["overall_average_text_rating"], 2)
+        context = {
+            "datas" : items
+        }
+    
+    return render(request, "saveKannadaLambani.html", context)
+
+def saveLambaniKannada(request):
+    if request.method == 'POST':
+        # with open('D:\S2ST\Language Translation Feedback\media\/text.json', 'r') as f1:
+        with open(settings.MEDIA_ROOT + 'Lambani_Kannada.json', 'r') as f1:
+            json_object = json.load(f1)
+
+        values_dict = {
+            'option1': 1, 
+            'option2': 2, 
+            'option3': 3, 
+            'option4': 4, 
+            'option5': 5
+            }
+        response = request.POST.dict()
+        result = dict()
+        text_total = 0
+        counter = 0
+        for key, value in response.items():
+            if key != 'csrfmiddlewaretoken':
+                if 'text' in key:
+                    text_total += values_dict[value]
+                    counter += 1
+                result[key] = values_dict[value]
+        result['average_text_rating'] = float(text_total)/float(counter)
+        json_object['count'] = json_object['count'] + 1
+        json_object['users'].append(result)
+        total_text_rating = 0
+        for user in json_object['users']:
+            total_text_rating += user['average_text_rating']
+        items = dict()
+        json_object["overall_average_text_rating"] = float(total_text_rating)/float(json_object['count'])
+
+        # with open('D:\S2ST\Language Translation Feedback\media\/text.json', 'w') as f1:
+        with open(settings.MEDIA_ROOT + 'Lambani_Kannada.json', 'w') as f1:
+            json.dump(json_object, f1)
+        
+        items['counter'] = json_object['count']
+        items["overall_average_text_rating"] = round(json_object["overall_average_text_rating"], 2)
+        context = {
+            "datas" : items
+        }
+    
+    return render(request, "saveLambaniKannada.html", context)
+
 def saveAudio(request):
     if request.method == 'POST':
         # with open('D:\S2ST\Language Translation Feedback\media\/audio.json', 'r') as f1:
@@ -148,11 +276,19 @@ def rating(request):
     # with open('D:\S2ST\Language Translation Feedback\media\/text.json', 'r') as f1:
     with open(settings.MEDIA_ROOT + 'text.json', 'r') as f1:
         text_json_object = json.load(f1)
+    with open(settings.MEDIA_ROOT + 'Kannada_Lambani.json', 'r') as f1:
+        kannada_lambani_json_object = json.load(f1)
+    with open(settings.MEDIA_ROOT + 'Lambani_Kannada.json', 'r') as f1:
+        lambani_kannada_json_object = json.load(f1)
     items = dict()
     items['text_counter'] = text_json_object['count']
     items['audio_counter'] = audio_json_object['count']
     items["overall_average_text_rating"] = round(text_json_object["overall_average_text_rating"], 2)
     items["overall_average_audio_rating"] = round(audio_json_object["overall_average_audio_rating"], 2)
+    items['kannada_lambani_counter'] = kannada_lambani_json_object['count']
+    items["overall_average_kannada_lambani_rating"] = round(kannada_lambani_json_object["overall_average_text_rating"], 2)
+    items['lambani_kannada_counter'] = lambani_kannada_json_object['count']
+    items["overall_average_lambani_kannada_rating"] = round(lambani_kannada_json_object["overall_average_text_rating"], 2)
     context = {
         "datas" : items
     }
